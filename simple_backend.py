289fa8dae46509ea45854c8054631ca03e959848
diosdadoalex <63257386+diosdadoalex@users.noreply.github.com>
2021-01-15 18:08:20 -0800

    Step 4: implemented CORS

---
+++ simple_backend.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+users = { 
+   'users_list' :
+   [
+      { 
+         'id' : 'xyz789',
+         'name' : 'Charlie',
+         'job': 'Janitor',
+      },
+      {
+         'id' : 'abc123', 
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id' : 'ppp222', 
+         'name': 'Mac',
+         'job': 'Professor',
+      }, 
+      {
+         'id' : 'yat999', 
+         'name': 'Dee',
+         'job': 'Aspring actress',
+      },
+      {
+         'id' : 'zap555', 
+         'name': 'Dennis',
+         'job': 'Bartender',
+      }
+   ]
+}
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      search_job = request.args.get('job')
+      if search_username and search_job:
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username and user['job'] == search_job:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+
+@app.route('/users/<id>', methods = ['GET', 'DELETE'])
+def get_user(id):
+   if request.method == 'GET':
+      if id :
+         for user in users['users_list']:
+            if user['id'] == id:
+               return user
+         return ({})
+      return users
+   elif request.method == 'DELETE':
+   	  if id :
+   	  	 subdict = {'users_list': []}
+   	  	 for user in users['users_list']:
+   	  	 	if user['id'] != id:
+   	  	 		subdict['users_list'].append(user)
+   	  	 return subdict
+   	  return users
--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table'
 import Form from './Form.js';
+import axios from 'axios';
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
@@ -10,18 +11,37 @@ function MyApp() {
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
     </div>
-  ) 
+    ) 
 
     function removeOneCharacter (index) {
       const updated = characters.filter((character, i) => {
          return i !== index
       });
       setCharacters(updated);
-   }
+    }
 
-   function updateList(person) {
-  setCharacters([...characters, person]);
-}
+    function updateList(person) {
+      setCharacters([...characters, person]);
+    }
+
+    async function fetchAll(){
+      try {
+        const response = await axios.get('http://localhost:5000/users');
+        return response.data.users_list;     
+      }
+      catch (error){
+        //We're not handling errors. Just logging into the console.
+        console.log(error); 
+        return false;         
+      }
+    }
+
+    useEffect(() => {
+      fetchAll().then( result => {
+        if (result)
+          setCharacters(result);
+      });
+    }, [] );
 
 }
 
